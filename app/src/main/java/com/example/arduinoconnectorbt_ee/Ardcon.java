42d342ec9a6b1b4fa1c3d863e6359a732b94d357
teodorkasap <erol.erten@gmail.com>
2020-06-11 17:56:50 +0300

    Ardcon.java

--- app/src/main/java/com/example/arduinoconnectorbt_ee/Ardcon.java
+++ app/src/main/java/com/example/arduinoconnectorbt_ee/Ardcon.java
@@ -8,8 +8,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+//import android.support.design.widget.FloatingActionButton;
+//import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -72,11 +72,11 @@ public class Ardcon extends AppCompatActivity {
             public void onClick(View v) {
                 Log.i("[BLUETOOTH]", "Attempting to send data");
                 if (mmSocket.isConnected() && btt != null) { //if we have connection to the bluetoothmodule
-                    if(relayFlag){
+                    if (relayFlag) {
                         String sendtxt = "RY";
                         btt.write(sendtxt.getBytes());
                         relayFlag = false;
-                    }else{
+                    } else {
                         String sendtxt = "RN";
                         btt.write(sendtxt.getBytes());
                         relayFlag = true;
@@ -87,9 +87,9 @@ public class Ardcon extends AppCompatActivity {
                     new Thread(new Runnable() {
                         @Override
                         public void run() {
-                            try{
+                            try {
                                 Thread.sleep(4000);
-                            }catch(InterruptedException e){
+                            } catch (InterruptedException e) {
                                 return;
                             }
 
@@ -111,10 +111,10 @@ public class Ardcon extends AppCompatActivity {
         bta = BluetoothAdapter.getDefaultAdapter();
 
         //if bluetooth is not enabled then create Intent for user to turn it on
-        if(!bta.isEnabled()){
+        if (!bta.isEnabled()) {
             Intent enableBTIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBTIntent, REQUEST_ENABLE_BT);
-        }else{
+        } else {
             initiateBluetoothProcess();
         }
     }
@@ -123,14 +123,14 @@ public class Ardcon extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(resultCode == RESULT_OK && requestCode == REQUEST_ENABLE_BT){
+        if (resultCode == RESULT_OK && requestCode == REQUEST_ENABLE_BT) {
             initiateBluetoothProcess();
         }
     }
 
-    public void initiateBluetoothProcess(){
+    public void initiateBluetoothProcess() {
 
-        if(bta.isEnabled()){
+        if (bta.isEnabled()) {
 
             //attempt to connect to bluetooth module
             BluetoothSocket tmp = null;
@@ -141,22 +141,26 @@ public class Ardcon extends AppCompatActivity {
                 tmp = mmDevice.createRfcommSocketToServiceRecord(MY_UUID);
                 mmSocket = tmp;
                 mmSocket.connect();
-                Log.i("[BLUETOOTH]","Connected to: "+mmDevice.getName());
-            }catch(IOException e){
-                try{mmSocket.close();}catch(IOException c){return;}
+                Log.i("[BLUETOOTH]", "Connected to: " + mmDevice.getName());
+            } catch (IOException e) {
+                try {
+                    mmSocket.close();
+                } catch (IOException c) {
+                    return;
+                }
             }
 
             Log.i("[BLUETOOTH]", "Creating handler");
-            mHandler = new Handler(Looper.getMainLooper()){
+            mHandler = new Handler(Looper.getMainLooper()) {
                 @Override
                 public void handleMessage(Message msg) {
                     //super.handleMessage(msg);
-                    if(msg.what == ConnectedThread.RESPONSE_MESSAGE){
-                        String txt = (String)msg.obj;
-                        if(response.getText().toString().length() >= 30){
+                    if (msg.what == ConnectedThread.RESPONSE_MESSAGE) {
+                        String txt = (String) msg.obj;
+                        if (response.getText().toString().length() >= 30) {
                             response.setText("");
                             response.append(txt);
-                        }else{
+                        } else {
                             response.append("\n" + txt);
                         }
                     }
@@ -164,7 +168,7 @@ public class Ardcon extends AppCompatActivity {
             };
 
             Log.i("[BLUETOOTH]", "Creating and running Thread");
-            btt = new ConnectedThread(mmSocket,mHandler);
+            btt = new ConnectedThread(mmSocket, mHandler);
             btt.start();
 
 
